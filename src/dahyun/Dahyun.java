989e93de09fb7d05a1ff3a969f5cbc1443bb26c5
Rhodz <Rhodz@DESKTOP-INQKN1K>
2024-02-15 19:02:59 +0800

    Dahyun

--- src/dahyun/Dahyun.java
+++ src/dahyun/Dahyun.java
@@ -15,6 +15,17 @@ public class Dahyun {
      */
     public static void main(String[] args) {
         
+        int[] practice = {10, 99, 99999, 414231, 20};
+        
+        System.out.println("Array Elements: " + practice[0] + ", " + practice[1] + ", " + practice[2] 
+                + ", " + practice[3] + ", " + practice[4]);
+        
+        int practice_sum = practice[0] + practice[1] + practice[2] + practice[3] + practice[4];
+        System.out.println("Sum of all arrays: " + practice_sum);
+        
+        int max = Math.max(practice[2], Math.max(practice[1], Math.max(practice[4], Math.max(practice[3], practice[0]))));
+        System.out.println("Maximum Value: " + max);
+        
     }
     
 }
